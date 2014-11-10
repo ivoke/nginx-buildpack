feede58a996d73e4741b60d7ad35cbb83c0b59ed
Michael Dietz <michael.dietz@ivoke.de>
2014-11-10 15:31:48 +0100

    Update to latest version of randydotsmith original buildpack

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -8,38 +8,34 @@ PCRE_VERSION=8.33
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.gz
-#pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 
-temp_dir=$(mktemp -d /tmp/vulcan_nginx.XXXXXXXXXX)
+temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
-cleanup() {
-  echo "Cleaning up $temp_dir"
-  cd /
-  rm -rf "$temp_dir"
-}
-trap cleanup EXIT
-
-script_dir=$(cd $(dirname $0); pwd)
-vulcan_archive_result=$temp_dir/nginx-${NGINX_VERSION}-built-with-vulcan.tar.gz
-untarring_dir=$temp_dir/untarring
-nginx_binary_drop_dir=$script_dir/../bin
+echo "Serving files from /tmp on $PORT"
+cd /tmp
+python -m SimpleHTTPServer $PORT &
 
 cd $temp_dir
 echo "Temp dir: $temp_dir"
 
 echo "Downloading $nginx_tarball_url"
-curl $nginx_tarball_url | tar xzf -
+curl -L $nginx_tarball_url | tar xzv
 
 echo "Downloading $pcre_tarball_url"
-(cd nginx-${NGINX_VERSION} && curl $pcre_tarball_url | tar xzf -)
+(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
 
-vulcan build -o ${vulcan_archive_result} -s nginx-${NGINX_VERSION} -v -p /tmp/nginx -c "./configure --with-pcre=pcre-${PCRE_VERSION} --with-http_gzip_static_module --with-http_realip_module --prefix=/tmp/nginx && make install"
+(
+  cd nginx-${NGINX_VERSION}
+  ./configure \
+    --with-pcre=pcre-${PCRE_VERSION} \
+    --prefix=/tmp/nginx \
+    --with-http_gzip_static_module \
+    --with-http_realip_module
+  make install
+)
 
-echo "Extracting the nginx binary into the buildback"
-mkdir -p $untarring_dir
-cd $untarring_dir
-tar -xf $vulcan_archive_result
-mkdir -p $nginx_binary_drop_dir
-cp sbin/nginx $nginx_binary_drop_dir
-
-echo "Copied the nginx binary into $(cd $nginx_binary_drop_dir; pwd)"
+while true
+do
+  sleep 1
+  echo "."
+done
